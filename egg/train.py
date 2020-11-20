f72cce0a1844f72d6693d353848dc2a40d6d72a2
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-20 23:53:23 +0800

    data loading

--- egg/train.py
+++ egg/train.py
@@ -4,11 +4,17 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+# /home/kenny/tf/SR-ARE-train
 file = open("../../source_file/csci_data/SR-ARE-train/names_onehots.pickle","rb")
 data = pickle.load(file)
 
 # we want to sep the onehot and names
-onthot = data["onehots"]
-name = data["names"]
+onehot = data["onehots"]
 
-print(onthot[1])
+file = open("../../source_file/csci_data/SR-ARE-train/names_labels.txt","r")
+content = np.loadtxt(file,delimiter=",",dtype="str")
+
+toxic = []
+for i in range(len(content)):
+    toxic.append( int(content[i][1]) )
+print(toxic)
